2f5f35bdf6317a4bd7ff8f0b4202390e6103fc41
Xin Long <xinlong-@hotmail.com>
2024-10-05 04:52:46 +0800

    Added app icons

--- constants/images.js
+++ constants/images.js
@@ -1,11 +1,9 @@
 import path from "../assets/images/path.png";
 import logo from "../assets/images/logo.png";
 import logoSmall from "../assets/images/logo-small.png";
-import cycling from "../assets/images/cycling.png";
-import ball from "../assets/images/ball.png";
 import favicon from "../assets/images/favicon.png";
 import icon from "../assets/images/icon.png";
 import adaptiveIcon from "../assets/images/icon.png";
 import splash from "../assets/images/splash.png";
 
-export default { path, logo, logoSmall, cycling, ball, favicon, icon, adaptiveIcon, splash };
+export default { path, logo, logoSmall, favicon, icon, adaptiveIcon, splash };
