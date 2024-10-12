3b749c5ead4e54af464b9520a09a441cefeecfa9
Xin Long <xinlong-@hotmail.com>
2024-10-13 00:48:23 +0800

    Update expo and add jest unit testing library

---
+++ __tests__/login-test.ts
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react-native';
+
+import login from '@/app/(auth)/login';
+
+describe('<login />', () => {
+  test('Text renders correctly on HomeScreen', () => {
+    
+  });
+});
