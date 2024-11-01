dff74a09e963fd7deadfa787bd261113e510297c
Xin Long <xinlong-@hotmail.com>
2024-11-02 06:50:42 +0800

    Added date to each task for easier differentiation from one another

--- app/(drawer)/tasks.tsx
+++ app/(drawer)/tasks.tsx
@@ -355,6 +355,20 @@ const Tasks: React.FC<TaskData> = () => {
               <Fontisto name="doctor" size={18} color={(colorScheme === 'dark' ? '#fff' : '#000')} style={{ marginRight: 10 }} />
               <Text className='text-sm mb-3 text-dark dark:text-light'>{item.staff.username}</Text>
             </View>
+            <View className='ml-2 flex-row'>
+              <Fontisto name="date" size={18} color={(colorScheme === 'dark' ? '#fff' : '#000')} style={{ marginRight: 10 }} />
+              <Text className='text-sm mb-3 text-dark dark:text-light'>
+                {`${new Date(item.task.createdAt).toLocaleDateString('en-US', {
+                  year: 'numeric',
+                  month: 'long',
+                  day: 'numeric',
+                })}, ${new Date(item.task.createdAt).toLocaleTimeString('en-US', {
+                  hour: 'numeric',
+                  minute: 'numeric',
+                  hour12: true,
+                })}`}
+              </Text>
+            </View>
             <View className='absolute bottom-3 right-0'>
               {
                 item.status === 'Completed' ? (
